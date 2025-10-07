0a68e9687b335a5b8387f7b09cd0cac5b4956ee6
jan <162505645+jann33k@users.noreply.github.com>
2025-10-07 21:36:32 +0200

    Create dzielenie.cpp

---
+++ Klasa_3/Informatyka/18.09.2025/dzielenie.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int a,b;
+	float d;
+	cout<<"Podaj a i b"<<endl;
+	cin>>a;
+	cin>>b;
+	if (d==0) {
+	cout<<"ERROR"<<endl;
+	}
+	else {
+	d=a/b;
+	cout<<"wynik = "<<d;
+	}
+	return 0;
+}
